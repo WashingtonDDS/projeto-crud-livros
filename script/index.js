5939070dbceeefdb6a54a1739bcfba7e4665e831
WashingtonDDS <washington.DDS@hotmail.com>
2025-01-08 21:35:04 -0300

    add settings for live server, update book data structure, and implement movie listing feature

---
+++ script/index.js
@@ -0,0 +1,16 @@
+const url = "http://localhost:3000/livros";
+const ulElemento = document.querySelector("#filmes");
+
+const listarLivros = async () => {
+  const resposta = await axios.get(url);
+  resposta.data.forEach((filme) => {
+    criarFilme(filme);
+  });
+};
+
+const criarFilme = (filme) => {
+  const li = document.createElement("li");
+  li.textContent = filme.titulo;
+  ulElemento.appendChild(li);
+};
+listarLivros();
