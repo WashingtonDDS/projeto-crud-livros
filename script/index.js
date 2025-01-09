f4e48ed4aef77566aa6252a0987d81f0231525b0
WashingtonDDS <washington.DDS@hotmail.com>
2025-01-09 19:03:23 -0300

    feat:Update movie listing feature and enhance form submission handling

--- script/index.js
+++ script/index.js
@@ -1,16 +1,26 @@
 const url = "http://localhost:3000/livros";
 const ulElemento = document.querySelector("#filmes");
+const elementoInputTitle = document.querySelector("input[name='titulo']");
 
 const listarLivros = async () => {
   const resposta = await axios.get(url);
   resposta.data.forEach((filme) => {
-    criarFilme(filme);
+    criarLiFilme(filme);
   });
 };
+
+listarLivros();
 
 const criarLiFilme = (filme) => {
   const li = document.createElement("li");
   li.textContent = filme.titulo;
   ulElemento.appendChild(li);
 };
-listarLivros();
+
+const cadastrarFilme = async (event) => {
+  event.preventDefault();
+  const resposta = await axios.post(url, {
+    titulo: elementoInputTitle.value,
+  });
+  console.log(resposta);
+};
