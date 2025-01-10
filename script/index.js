82e60c12092d8974f8a87a90989896a606734387
WashingtonDDS <washington.DDS@hotmail.com>
2025-01-09 21:45:15 -0300

    feat:Enhance movie management by adding selection functionality and updating db.json

--- script/index.js
+++ script/index.js
@@ -1,6 +1,7 @@
 const url = "http://localhost:3000/livros";
 const ulElemento = document.querySelector("#filmes");
 const elementoInputTitle = document.querySelector("input[name='titulo']");
+let idSelecionado = null;
 
 const listarLivros = async () => {
   const resposta = await axios.get(url);
@@ -14,7 +15,18 @@ listarLivros();
 const criarLiFilme = (filme) => {
   const li = document.createElement("li");
   li.textContent = filme.titulo;
+  li.id = filme.id;
+  selecionarFilme(li);
+
   ulElemento.appendChild(li);
+};
+
+const selecionarFilme = async (li) => {
+  li.addEventListener("click", (event) => {
+    idSelecionado = event.target.id;
+    elementoInputTitle.value = event.target.textContent;
+    console.log(idSelecionado);
+  });
 };
 
 const cadastrarFilme = async (event) => {
@@ -23,4 +35,13 @@ const cadastrarFilme = async (event) => {
     titulo: elementoInputTitle.value,
   });
   console.log(resposta);
+};
+
+const alterarFilme = async (event) => {
+  if (!idSelecionado) {
+    return;
+  }
+  const resposta = await axios.put(`${url}/${idSelecionado}`, {
+    titulo: elementoInputTitle.value,
+  });
 };
